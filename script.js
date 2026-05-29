d4d714e4e314c5327a2ac9487755fdde42a302bf
Yaw Phame Snr <162208309+Yawphame@users.noreply.github.com>
2026-05-29 14:24:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// INVOICENEST SCRIPT
-
-// CREATE INVOICE FORM
+// INVOICENEST APP
 
 const invoiceForm = document.querySelector(".invoice-form");
 
@@ -10,7 +8,66 @@ if(invoiceForm){
 
     event.preventDefault();
 
-    alert("Invoice Generated Successfully!");
+    // GET FORM VALUES
+
+    const clientName =
+      document.querySelector('input[placeholder="Enter client name"]').value;
+
+    const clientEmail =
+      document.querySelector('input[placeholder="Enter client email"]').value;
+
+    const amount =
+      document.querySelector('input[placeholder="$0.00"]').value;
+
+    const dueDate =
+      document.querySelector('input[type="date"]').value;
+
+    const description =
+      document.querySelector("textarea").value;
+
+    const status =
+      document.querySelector("select").value;
+
+
+    // CREATE INVOICE OBJECT
+
+    const invoice = {
+      clientName,
+      clientEmail,
+      amount,
+      dueDate,
+      description,
+      status
+    };
+
+
+    // GET OLD INVOICES
+
+    let invoices =
+      JSON.parse(localStorage.getItem("invoices")) || [];
+
+
+    // ADD NEW INVOICE
+
+    invoices.push(invoice);
+
+
+    // SAVE TO LOCAL STORAGE
+
+    localStorage.setItem(
+      "invoices",
+      JSON.stringify(invoices)
+    );
+
+
+    // SUCCESS MESSAGE
+
+    alert("Invoice Saved Successfully!");
+
+
+    // RESET FORM
+
+    invoiceForm.reset();
 
   });
 
